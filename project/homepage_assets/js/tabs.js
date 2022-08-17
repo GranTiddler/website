04957b591ab73b0982b262083d05e3ae2fe13555
skAdOOdlEs7 <grantdriddle7@gmail.com>
2022-08-16 18:59:03 -0600

    fixed styling

---
+++ project/homepage_assets/js/tabs.js
@@ -0,0 +1,32 @@
+function games() {
+    var x = document.getElementById("about");
+    var y = document.getElementById("game");
+    var z = document.getElementById("main");
+  
+     
+    y.style.display = "block";
+    x.style.display = "none";
+    z.style.display = "none";
+  }
+  
+  function about() {
+    var x = document.getElementById("about");
+    var y = document.getElementById("game");
+    var z = document.getElementById("main");
+  
+     
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  }
+  
+  function main() {
+    var x = document.getElementById("about");
+    var y = document.getElementById("game");
+    var z = document.getElementById("main");
+  
+     
+    z.style.display = "block";
+    y.style.display = "none";
+    x.style.display = "none";
+  }
